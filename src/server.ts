1bec29afc23b8e839b19eabcd0aaacab15bdc86d
Martin Liu <martin.liu@ucalgary.ca>
2026-06-15 23:33:10 -0600

    feat(server): incoming reqs now need a api key

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import Fastify, { FastifyError, FastifyRequest } from 'fastify';
+import Fastify, { FastifyError, FastifyReply, FastifyRequest } from 'fastify';
 import 'dotenv/config';
 import { connectorRoutes } from './routes/connectors/connectors.js';
 import { log, LOG_LEVEL } from './utils/logger.js';
@@ -6,8 +6,18 @@ import { prismaPlugin } from './plugins/prisma.js';
 
 const fastify = Fastify({ logger: true });
 
-fastify.addHook('onError', async (request: FastifyRequest, _, error: FastifyError) => {
+fastify.addHook('onRequest', async (request, reply) => {
+  if (request.headers['x-api-key'] !== process.env.CONNECTOR_API_KEY) {
+    log(`Rejected backend hit from [${request.host}] due to no api key`, LOG_LEVEL.ERROR);
+    reply.code(401).send({ message: 'UNAUTHORIZED' });
+  }
+});
+
+fastify.addHook('onError', async (request: FastifyRequest, reply: FastifyReply, error: FastifyError) => {
   await log(`${request.method} ${request.url} - ${error.message}`, LOG_LEVEL.ERROR);
+  reply.code(500).send({
+    error: error.message
+  });
 });
 
 fastify.register(prismaPlugin);
